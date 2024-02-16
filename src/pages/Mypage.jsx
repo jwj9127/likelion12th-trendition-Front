397bd3a81af0bcf876b9ca8a197df98198b4b79d
jwj9127 <jwj9127@naver.com>
2024-02-16 22:29:55 +0900

    마이페이지 완료

--- src/pages/Mypage.jsx
+++ src/pages/Mypage.jsx
@@ -1,23 +1,32 @@
 import { React } from "react";
 import Navigation from "../component/Navigation";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGear } from "@fortawesome/free-solid-svg-icons";
+import logo2 from "../imgs/logo2.png";
+import '../css/Mypage.css';
+import { Link } from "react-router-dom";
 
 export default function Mypage() {
     return (
         <div>
             <Navigation></Navigation>
-            <div>
-                <div></div>
-                <div></div>
-                <div></div>
+            <div className="mypage_top_setting">
+                <div className="mypage_top_setting_flex">
+                    <div>
+                        <img className="mypage_logoBox_logo" src={logo2} alt="logo"></img>
+                        <p>마라톤</p>
+                    </div>
+                    <p>@brunopham</p>
+                    <Link to={'/follow'}><FontAwesomeIcon icon={faGear} size="2x"/></Link>
+                    </div>
+                <div className="mypage_main_profile">
+                    <div className="mypage_main_img"></div>
+                    <p>Bruno Pham</p>
+                </div>
             </div>
-            <div>
-                <div></div>
-                <p></p>
-                <p></p>
-            </div>
-            <div>
-                <p><i></i></p>
-                <p><i></i></p>
+            <div className="mypage_followBox">
+                <p>220<i>Followers</i></p>
+                <p>150<i>Following</i></p>
             </div>
         </div>
     );
