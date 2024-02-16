e413ce000e2e87369b62b0d9968a91b169352856
jungbug <xbox1111@naver.com>
2024-02-16 15:36:42 +0900

    add: Follow

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import SignUp from "./pages/SignUp.jsx";
 import SignUpNext from "./pages/SignUpNext.jsx";
 import Search from "./pages/Search.jsx";
 import Mypage from "./pages/Mypage.jsx";
+import Follow from "./pages/Follow.jsx";
+import Follower from "./pages/Follower.jsx";
 
 function App() {
     return (
@@ -21,6 +23,8 @@ function App() {
                 <Route path="/main" element={<Main />} />
                 <Route path="/search" element={<Search />} />
                 <Route path="/mypage" element={<Mypage />} />
+                <Route path="/follow" element={<Follow />} />
+                <Route path="/follower" element={<Follower />} />
             </Routes>
         </div>
     );
