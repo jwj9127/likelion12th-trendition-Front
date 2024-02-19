e03fad9824c0aa8299377f0f26f93145a8e9650a
jwj9127 <jwj9127@naver.com>
2024-02-19 19:57:50 +0900

    메인화면 수정

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -5,9 +5,11 @@ import HexagonGraph from "../component/HexagonGraph";
 import BeforeSet from "../component/BeforeSet";
 import logo2 from "../imgs/logo2.png";
 import pencil from "../imgs/pencil.png";
+import garbage from "../imgs/garbage.png";
 import Swal from "sweetalert2";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGear } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
 
 function TopBar({ goalCount }) {
     const achievementRate = goalCount * 20;
@@ -35,46 +37,176 @@ function TopBar({ goalCount }) {
 }
 
 function GoalCheck({ level }) {
+    const [isChecked, setIsChecked] = useState(false);
+    const subgoals = window.localStorage.getItem('subgoals');
+
+    const checkBox = () =>{
+        const completed = document.getElementById('subGoal-check-completed').value;
+
+        Swal.fire({
+            title: '목표 완료',
+            showCancelButton: true,
+            confirmButtonText: "잠굼",
+            cancelButtonText: "취소",
+        }).then((result) => {
+            if (result.isConfirmed) {
+                try{
+                    axios({
+                        method: 'post',
+                        url: '/home/goal/create',
+                        data: { completed: true }
+                    }).then(() => {
+                        setIsChecked(true);
+                    })
+                }catch (err) {
+                    console.error(err);
+                }
+            }
+            if (!(result.isConfirmed)){
+                setIsChecked(false);
+            }
+        })
+    }
+
+    // try{
+    //     axios({
+    //         method: 'get',
+    //         url: '/home'
+    //     }).then((result) => {
+    //         window.localStorage.setItem('subgoals', result.data.subgoals);
+    //     })
+    // }catch(err){
+    //     console.error(err);
+    // }
     return (
         <div className="subGoal">
-            <input className="subGoal-check" type="checkbox"></input>
+            <input id="subGoal-check-completed" className={isChecked ? "subGoal-check-checked" : "subGoal-check"} type="checkbox" onClick={checkBox} disabled={isChecked}></input>
             <div className="subGoal-text">
-                {`Lv - ${level} : 목표설정`}
+                {`Lv - ${level} : ${subgoals ? subgoals.detail : "목표 설정"}`}
                 <img
                     className="subGoal-pencil"
                     src={pencil}
                     onClick={TargetGoals}
+                    style={{ marginLeft: subgoals===null ? "60%" : "50%" }}
                 />
+                {subgoals && (
+                    <img
+                        className="subGoal-garbage"
+                        src={garbage}
+                        onClick={DeleteGoals}
+                    />
+                )}
             </div>
         </div>
     );
 }
 
-function TargetGoals() {
+function DeleteGoals() {
+    const subgoals = window.localStorage.getItem('subgoals');
     Swal.fire({
-        html: `
-        <div class="spec-modal">
-        <div class="spec-content">
-            <input id="stack1" class="swal2-input" style="outline: none;" placeholder="목표를 설정해주세요">
-        </div>
-    </div>
-        `,
+        title: '삭제 하시겠습니까?',
         showCancelButton: true,
-        confirmButtonText: "목표 설정",
+        confirmButtonText: "삭제",
         cancelButtonText: "취소",
-    });
+    }).then((result) => {
+        if (result.isConfirmed) {
+            try{
+                axios({
+                    method: 'post',
+                    url: '/home/subgoal/delete',
+                    data: subgoals.subgoal_id
+                })
+            }catch (err) {
+                console.error(err);
+            }
+        }
+    })
+}
+
+function TargetGoals() {
+    const subgoals = window.localStorage.getItem('subgoals');
+
+    if(subgoals){
+        Swal.fire({
+            html: `
+            <div class="spec-modal">
+            <div class="spec-content">
+                <input id="stack" class="swal2-input" style="outline: none;" placeholder="목표를 수정해주세요">
+            </div>
+        </div>
+            `,
+            showCancelButton: true,
+            confirmButtonText: "목표 수정",
+            cancelButtonText: "취소",
+        }).then((result) => {
+            if (result.isConfirmed) {
+                const stack = document.getElementById('stack').value;
+                try{
+                    axios({
+                        method: 'put',
+                        url: '/home/subgoal/update/',
+                        data: stack
+                    })
+                }catch (err) {
+                    console.error(err);
+                }
+            }
+        });
+    }
+
+    if(!subgoals){
+        Swal.fire({
+            html: `
+            <div class="spec-modal">
+            <div class="spec-content">
+                <input id="stack" class="swal2-input" style="outline: none;" placeholder="목표를 설정해주세요">
+            </div>
+        </div>
+            `,
+            showCancelButton: true,
+            confirmButtonText: "목표 설정",
+            cancelButtonText: "취소",
+        }).then((result) => {
+            if (result.isConfirmed) {
+                const stack = document.getElementById('stack').value;
+    
+                try{
+                    axios({
+                        method: 'post',
+                        url: '/home/subgoal/create/',
+                        data: stack
+                    })
+                }catch (err) {
+                    console.error(err);
+                }
+            }
+        });
+    }
 }
 
 function Goals() {
+    const id = window.localStorage.getItem('id');
+    const title = window.localStorage.getItem('title');
+    // try{
+    //     axios({
+    //         method: 'get',
+    //         url: '/home'
+    //     }).then((result) => {
+    //         window.localStorage.setItem('id', result.data.id);
+    //         window.localStorage.setItem('title', result.data.title);
+    //     })
+    // }catch(err){
+    //     console.error(err);
+    // }
     return (
         <div className="Goals">
             <select className="mainGoal-text">
-                <option>스펙 1 : 마라톤</option>
-                <option>스펙 2 : 필라테스</option>
-                <option>스펙 3 : 블로그 운영</option>
-                <option>스펙 4 : 영어 회화</option>
-                <option>스펙 5 : 요리</option>
-                <option>스펙 6 : 수영</option>
+                <option>스펙 : 마라톤</option>
+                <option>스펙 : 필라테스</option>
+                <option>스펙 : 블로그 운영</option>
+                <option>스펙 : 영어 회화</option>
+                <option>스펙 : 요리</option>
+                <option>스펙 : 수영</option>
             </select>
             {[1, 2, 3, 4, 5].map((level) => (
                 <GoalCheck key={level} level={level}></GoalCheck>
@@ -116,18 +248,27 @@ function TargetTag() {
         cancelButtonText: "취소",
         allowOutsideClick: false,
     }).then(() => {
-        document.querySelector(".HexagonGraphBox").innerHTML =
-            "<HexagonGraph></HexagonGraph>";
     });
 }
 
 function HexagonGraphBox() {
+    const id = window.localStorage.getItem('id');
+    const [isSet, setIsSet] = useState(false);
+    // try{
+    //     axios({
+    //         method: 'get',
+    //         url: '/home'
+    //     }).then((result) => {
+    //         window.localStorage.setItem('id', result.data.id);
+    //         setIsSet(true);
+    //     })
+    // }catch(err){
+    //     console.error(err);
+    // }
     return (
         <div className="HexagonGraphBox">
-            {/*설정 전 헥사곤 그래프 대체 이미지*/}
-            {/*<BeforeSet onClick={TargetTag} />*/}
-            {/*설정 후 헥사곤 그래프*/}
-            <HexagonGraph />
+            {/*설정 여부에 따른 조건부 렌더링*/}
+            {isSet ? <HexagonGraph /> : <BeforeSet onClick={TargetTag} />}
         </div>
     );
 }
